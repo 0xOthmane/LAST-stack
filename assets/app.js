36c1fac709162a321e03aa4cf632e27047b6c3d1
0xOthmane <0xothodor@proton.me>
2024-01-05 16:21:42 +0100

    feat: add view transitions

--- assets/app.js
+++ assets/app.js
@@ -1,16 +1,51 @@
-import './bootstrap.js';
+import "./bootstrap.js";
 /*
  * Welcome to your app's main JavaScript file!
  *
  * This file will be included onto the page via the importmap() Twig function,
  * which should already be in your base.html.twig.
  */
-import './styles/app.css'
-import alienGreeting from './lib/alien-greeting.js'
-import * as Turbo from '@hotwired/turbo'
+import "./styles/app.css";
+import alienGreeting from "./lib/alien-greeting.js";
+import * as Turbo from "@hotwired/turbo";
+import {
+  shouldPerformTransition,
+  performTransition,
+} from "turbo-view-transitions";
 
-alienGreeting('Give us all your candy', false)
+alienGreeting("Give us all your candy", false);
 
-console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉')
+console.log("This log comes from assets/app.js - welcome to AssetMapper! 🎉");
 
 // Turbo.session.drive = false
+let skipNextRenderTransition = false;
+document.addEventListener("turbo:before-render", (event) => {
+  if (shouldPerformTransition() && !skipNextRenderTransition) {
+    event.preventDefault();
+
+    performTransition(document.body, event.detail.newBody, async () => {
+      await event.detail.resume();
+    });
+  }
+});
+
+document.addEventListener("turbo:load", () => {
+  // View Transitions don't play nicely with Turbo cache
+  if (shouldPerformTransition()) Turbo.cache.exemptPageFromCache();
+});
+
+document.addEventListener("turbo:before-frame-render", (event) => {
+  if (
+    shouldPerformTransition() &&
+    !event.target.hasAttribute("data-skip-transition")
+  ) {
+    event.preventDefault();
+    skipNextRenderTransition = true;
+    setTimeout(() => {
+      skipNextRenderTransition = false;
+    }, 100);
+    performTransition(event.target, event.detail.newFrame, async () => {
+      await event.detail.resume();
+    });
+  }
+});
