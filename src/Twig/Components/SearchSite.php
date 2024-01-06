47f49f49406e2b3e11da1f019489baf2f7eecaf8
0xOthmane <0xothodor@proton.me>
2024-01-06 20:16:20 +0100

    feat: add live component

---
+++ src/Twig/Components/SearchSite.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Twig\Components;
+
+use App\Entity\Voyage;
+use App\Repository\VoyageRepository;
+use Symfony\UX\LiveComponent\Attribute\AsLiveComponent;
+use Symfony\UX\LiveComponent\Attribute\LiveProp;
+use Symfony\UX\LiveComponent\DefaultActionTrait;
+use Symfony\UX\TwigComponent\Attribute\AsTwigComponent;
+
+#[AsLiveComponent]
+class SearchSite
+{
+    use DefaultActionTrait;
+
+    #[LiveProp(writable: true)]
+    public string $query = '';
+
+    public function __construct(private VoyageRepository $voyageRepository)
+    {
+    }
+
+    /**
+     * @return Voyage[]
+     */
+    public function voyages(): array
+    {
+        if(!$this->query) return [];
+        return $this->voyageRepository->findBySearch($this->query, [], 10);
+    }
+}
